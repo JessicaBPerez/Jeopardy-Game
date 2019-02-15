b56e91136b3f5b41b8e58d07d9cd4d3f2b974e26
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-14 22:12:45 -0500

    click function works on whole game

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     //$("body").css("background", "blue");
-    $(".bb500").on("click", function() {
+    $("#wholeGame").on("click", function() {
         console.log("hello");
     })
 
