b571e1aa966881c221037b927d3e862ae4b893c0
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-14 22:10:10 -0500

    on click works on Breaking Bad 500

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    $("body").css("background", "blue");
+    //$("body").css("background", "blue");
+    $(".bb500").on("click", function() {
+        console.log("hello");
+    })
 
 
 });
