d569770e6480a55fec0b7ec34821194cfe778c94
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-15 01:05:33 -0500

    BB 500 question 1 done.

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
     //$("body").css("background", "blue");
-    /* $(".bb500").on("click", function() {
-         $(".bb500").hide();
-         $('.bb500backcard').show();
-         //console.log("hello");
-     })*/
+    $(".bb500").on("click", function() {
+        $(".bb500").hide();
+        $('.bb500').addClass("bb500backcard");
+        //console.log("hello");*/
+
+        /*if (balance <= 0) {
+            $("#checking").addClass("zero");
+        } else {
+            $("#checking").removeClass("zero");
+        }*/
+
+
+
+    })
 
 
 });
