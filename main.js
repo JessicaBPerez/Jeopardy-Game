c5fdb171870fcc2e6639c0d114db71eacac1a35a
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-15 22:05:05 -0500

    JS connected

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
 $(document).ready(function() {
-    let score = 0;
-    //$("body").css("background", "blue");
-    $(".bb500").on("click", function() {
-        $(".bb500").hide();
-        $('.bb500backcard').removeClass('hidden');
-        $(".A").on('click', function() {
-            $('.bb500backcard').addClass('hidden');
-            let value = $("A").val();
-            if ($("A").val() = "A") {
-                score += parseInt(value);
-            }
-        })
-
-        /*if (balance <= 0) {
-            $("#checking").addClass("zero");
-        } else {
-            $("#checking").removeClass("zero");
-        }*/
-
-
-
-    })
-
-
-});
+    $("body").css("background", "red");
+})
