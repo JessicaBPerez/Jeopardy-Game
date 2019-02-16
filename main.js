a1287a794873f9e9cfa4732113964d7aa88a1c89
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-15 23:19:44 -0500

    data transfered to Player 1 score, numbers to display next

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ $(document).ready(function() {
         $(".bb500backcard").removeClass("hidden");
         $("button").on("click", function() {
             $(".bb500backcard").addClass("hidden");
+
+            let value = $(".A").data("A", 500).val();
+            scorePlayer1 += parseInt(value);
+            $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+            console.log(scorePlayer1);
+            //$(".bb500").data();
+
         })
     })
 })
