605e0e74b7848b6f7e4cb947d70503313846162a
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-15 22:16:18 -0500

    bb500 works.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $("body").css("background", "red");
+    //$("body").css("background", "red");
+    var scorePlayer1 = 0;
+    var scorePlayer2 = 0;
+    $(".bb500").on("click", function() {
+        console.log("hi")
+    })
 })
