b2bc704924f738109e506fca31581ac1ba72b4c9
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-17 00:24:08 -0500

    both players now work

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     var scorePlayer1 = 0;
     var scorePlayer2 = 0;
     var playerOneTurn = true;
+
     $("#wholeGame").on("click", function() {
         $(".bb500").on("click", function() {
             $(".bb500").hide();
             $(".bb500backcard").removeClass("hidden");
             let value = $(".bb500").text();
+
             //Might need to move value.bb500 into global scale inside only #wholeGame function.
             $("button").on("click", function() {
                 $(".bb500backcard").addClass("hidden");
@@ -28,20 +30,53 @@ $(document).ready(function() {
                         $("#player2box .scorePlayer2").text(`${scorePlayer2}`);
                     }
                 }
-
-
-
-
-
-
-                /*if ($(this).hasClass("A")) {
-                    scorePlayer1 += parseInt(value);
-                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
-                } else {
-                    scorePlayer1 -= parseInt(value);
-                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
-                }*/
             })
         })
+
+
+
+
+
+
+
+        $(".bb400").on("click", function() {
+            $(".bb400").hide();
+            $(".bb400backcard").removeClass("hidden");
+            let value = $(".bb400").text();
+
+            //Might need to move value.bb400 into global scale inside only #wholeGame function.
+            $("button").on("click", function() {
+                $(".bb400backcard").addClass("hidden");
+                playerOneTurn = !playerOneTurn;
+                if (playerOneTurn == true) {
+                    if ($(this).hasClass("C")) {
+                        scorePlayer1 += parseInt(value);
+                        $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                    } else {
+                        scorePlayer1 -= parseInt(value);
+                        $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                    }
+                } else {
+                    if ($(this).hasClass("C")) {
+                        scorePlayer2 += parseInt(value);
+                        $("#player2box .scorePlayer2").text(`${scorePlayer2}`);
+                    } else {
+                        scorePlayer2 -= parseInt(value);
+                        $("#player2box .scorePlayer2").text(`${scorePlayer2}`);
+                    }
+                }
+            })
+        })
+
+
+
+
+
+
+
+
+
+
+
     })
 })
