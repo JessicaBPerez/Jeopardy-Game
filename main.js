73519f56da1501f92c60423fa49219236a4e393f
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-02-16 19:27:30 -0500

    testing player 2

--- main.js
+++ main.js
@@ -1,29 +1,59 @@
 $(document).ready(function() {
     var scorePlayer1 = 0;
     var scorePlayer2 = 0;
-    $(".bb500").on("click", function() {
-        $(".bb500").hide();
-        $(".bb500backcard").removeClass("hidden");
-        let value = $(".bb500").text();
+    var playerOneTurn = true;
+    $("#wholeGame").on("click", function() {
+        $(".bb500").on("click", function() {
+            $(".bb500").hide();
+            $(".bb500backcard").removeClass("hidden");
+            let value = $(".bb500").text();
+            //Might need to move value.bb500 into global scale inside only #wholeGame function.
+            $("button").on("click", function() {
+                $(".bb500backcard").addClass("hidden");
 
-        $("button").on("click", function() {
-            $(".bb500backcard").addClass("hidden");
+                if ($(this).hasClass("A")) {
+                    scorePlayer1 += parseInt(value);
+                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                } else {
+                    scorePlayer1 -= parseInt(value);
+                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                }
 
-            if ($(this).hasClass("A")) {
-                scorePlayer1 += parseInt(value);
-                $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
-            } else {
-                scorePlayer1 -= parseInt(value);
-                $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
-            }
+                console.log(playerOneTurn)
+            })
         })
+
     })
+
+
+
+    //This is the end of the .ready function.
 })
 
 
-/*//$(".bb500").data();
-let value = $(".bb500").data(".bb500", 500).value;
-scorePlayer1 += parseInt(value);
-$("#player1box .scorePlayer1").text(`${scorePlayer1}`);
-console.log(scorePlayer1);
-//$(".bb500").data();*/
+
+
+
+
+
+$("button").on("click", function() {
+            (".bb500backcard").addClass("hidden");
+            if (playerOneTurn) {
+                if ($(this).hasClass("A")) {
+                    scorePlayer1 += parseInt(value);
+                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                } else {
+                    scorePlayer1 -= parseInt(value);
+                    $("#player1box .scorePlayer1").text(`${scorePlayer1}`);
+                }
+
+
+            } else {
+                if ($(this).hasClass("A")) {
+                    scorePlayer2 += parseInt(value);
+                    $("#player2box .scorePlayer2").text(`${scorePlayer2}`);
+                } else {
+                    scorePlayer2 -= parseInt(value);
+                    $("#player2box .scorePlayer2").text(`${scorePlayer2}`);
+                }
+            }
